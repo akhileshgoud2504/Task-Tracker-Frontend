f3e5809725d837eab7ddfdc9ef4a17287f10fdd8
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-17 02:33:30 +0530

    [UPDATION]: updating cros origin

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(cors());
-// app.use(cors({
-//     origin:["http://localhost:3000/"]
-// }))
+app.use(cors({
+    origin:[
+            "http://localhost:3000/",
+            "http://mern-task-app.onrender.com"
+           ]
+}))
 app.use("/api/tasks",taskRoutes);
 
 
