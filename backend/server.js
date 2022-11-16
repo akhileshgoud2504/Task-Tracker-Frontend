54f0fb01588eb688d6e8e46bb120762b0c175126
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-17 01:47:09 +0530

    [UPDATION]: front end UI Updation

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const taskRoutes = require("./route/taskroute");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors());
+// app.use(cors({
+//     origin:["http://localhost:3000/"]
+// }))
 app.use("/api/tasks",taskRoutes);
 
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
         .connect(process.env.MONGO_URI)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import TaskList from "./compoenents/TaskList";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const URL = process.env.REACT_APP_SERVER_URL;
+
 function App() {
   return (
-    <div>
-      Hello world..
+    <div className="app">
+      <div className="task-container">
+        <TaskList/>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
